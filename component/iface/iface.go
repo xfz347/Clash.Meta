98ca8a7bf1737001d5488baf93223c9878e532a7
likuai2010 <likuai2010live.com>
2025-02-11 22:52:11 +0800

    suport ohos
    (cherry picked from commit 86df299685fbc2529dd7c3390dd8808b23fa1345)

--- component/iface/iface.go
+++ component/iface/iface.go
@@ -9,6 +9,8 @@ import (
 	"github.com/metacubex/mihomo/common/singledo"
 
 	"github.com/metacubex/bart"
+
+    "github.com/metacubex/mihomo/constant/features"
 )
 
 type Interface struct {
@@ -32,14 +34,22 @@ type ifaceCache struct {
 }
 
 var caches = singledo.NewSingle[*ifaceCache](time.Second * 20)
+var netInterfaces = []net.Interface{}
+// arkui 注入进来
+func setNetInterfaces(nets []net.Interface){
+    netInterfaces = nets
+}
 
 func getCache() (*ifaceCache, error) {
 	value, err, _ := caches.Do(func() (*ifaceCache, error) {
 		ifaces, err := net.Interfaces()
-		if err != nil {
-			return nil, err
+		if(!features.OHOS){
+		    if err != nil {
+                return nil, err
+            }
+		}else{
+		    ifaces = netInterfaces
 		}
-
 		cache := &ifaceCache{
 			ifMapByName: make(map[string]*Interface),
 			ifMapByAddr: make(map[netip.Addr]*Interface),
